cb0f1be9f4fe7f477f8199c0a3bb89cf2b0ceb10
fab9511 <fabianaczke@gmail.com>
2025-12-14 19:24:57 +0100

    naprawa działania bin.asm -- błąd przy instrukcji shr

--- asm_lib/utils/bin.asm
+++ asm_lib/utils/bin.asm
@@ -1,23 +1,24 @@
-        global _start
-        section .text
-	
-bin: 
-	;; funkcja zamiany liczby na napis liczby w systemie bin 
-	;; rdi - liczba
-	;; rsi - buffer
-	mov rax, rdi
-	mov cx, 8	; licznik funkcji
-.petla:	
-	mov bl, al	; kopia al na bl
-	mov dl, cl
-	sub dl, 1
-	shr bl, dl	; przesunięcie bitowe
-	and bl, 1 	; zerowanie wszystkich bitów oprócz pierwszego
-	add bl, 48	; dodanie '0' -- liczba w ascii
-	mov dl, 8	;
-	sub dl, cl	; przesunięcie bufora
-	mov [rsi+dl], bl	; 
-	loop .petla 	;wywołanie kolejnego obiegu pętli
-	mov bl, 0x0A    ; wartość ascii znaku nowej linii
+	global bin
+
+bin:
+    ;; funkcja zamiany liczby na napis liczby w systemie bin
+    ;; wynik zapisany w
+    ;; rdi - liczba
+    ;; rsi - buffer
+    mov rax, rdi
+    mov cx, 8   ; licznik funkcji
+.petla:
+    mov bl, al  ; kopia al na bl
+    mov dl, cl  ; kopia licznika pętli cl
+    sub cl, 1   ; ustalamy odpowiednie przesunięcie bitowe w prawo
+    shr bl, cl  ; przesunięcie bitowe w prawo
+    and bl, 1   ; zerowanie wszystkich bitów oprócz pierwszego
+    mov cl, dl  ; przywrócenie wartości licznika pętli cl
+    add bl, 48  ; dodanie '0' -- liczba w ascii
+    mov dl, 8   ; dl = 8
+    sub dl, cl  ; ustalamy odpowiednie przesunięcie bufora
+    mov [rsi+rdx], bl ; wartość bl na bufor
+    loop .petla     ;wywołanie kolejnego obiegu pętli
+    mov bl, 0x0A    ; wartość ascii znaku nowej linii
     mov [rsi+8], bl ; wartość bl na bufor
-	ret		; powrót z funkcji
+    ret     ; powrót z funkcji
